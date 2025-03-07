0dd82ebda177ce0ed217bb2e769a593a479be0ee
Edson Cássio Barcelos <59491303+edsonbarcellos02@users.noreply.github.com>
2025-03-07 17:34:28 -0300

    1.0.0.0

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <iframe class="embed-responsive-item" src="https://monitor.tecnospeed.com.br/?&filter-uf=rj#google_vignette" style="height: 600px; width: 99%; border: none;"></iframe>
+    <iframe class="embed-responsive-item" src="https://metallist.com.br/" style="height: 600px; width: 99%; border: none;"></iframe>
   </div>
 </template>
